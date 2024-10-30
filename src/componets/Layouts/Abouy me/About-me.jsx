5b49fa2ecd4c47977e0826186aaa0199b99cc2be
santi orozco <santioro1736@gmail.com>
2024-10-30 01:27:57 -0500

    Changes text in About me

--- src/componets/Layouts/Abouy me/About-me.jsx
+++ src/componets/Layouts/Abouy me/About-me.jsx
@@ -1,54 +1,87 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import { Computer, Atom, Smartphone } from 'lucide-react';
-import './About-Me.css';
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Computer, Atom, Smartphone } from "lucide-react";
+import "./AboutMe.css";
 
 export const AboutMe = () => {
   useEffect(() => {
     AOS.init({
       duration: 1000,
-      once: true,
-      offset: 100
+      offset: 100,
+      once: false,
+      mirror: true,
+      anchorPlacement: "center-bottom",
     });
+
+    window.addEventListener("scroll", () => {
+      AOS.refresh();
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {
+        AOS.refresh();
+      });
+    };
   }, []);
 
   return (
     <section className="expertise">
-      <h1 data-aos="fade-up">My Expertise</h1>
-      
+      <h1 data-aos="fade-up" data-aos-offset="200" data-aos-duration="1000">
+        Mi Experiencia
+      </h1>
+
       <div className="expertise-grid">
-        <div className="expertise-card" data-aos="fade-right" data-aos-duration="1000">
+        <div
+          className="expertise-card"
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          data-aos-mirror="true"
+        >
           <div className="card-icon">
             <Computer size={40} />
           </div>
-          <h2>Software Development</h2>
+          <h2>Desarrollo de Software</h2>
           <h3>
-            Experienced in both functional and OOP: Dart, Python, Java, JavaScript, TypeScript.
+            Experiencia en programación funcional: PHP, Python, Java,
+            JavaScript, TypeScript.
           </h3>
         </div>
 
-        <div className="expertise-card" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+        <div
+          className="expertise-card"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-delay="200"
+          data-aos-mirror="true"
+        >
           <div className="card-icon">
             <Atom size={40} />
           </div>
-          <h2>Frontend Dev</h2>
+          <h2>Desarrollo Frontend</h2>
           <h3>
-            Passionate about UI/UX. Over 5 years of development experience in HTML, CSS, JS, React and NextJS frameworks.
+            Apasionado por UI/UX. Más de 3 años de experiencia en desarrollo con
+            HTML, CSS, JS, React y frameworks NextJS.
           </h3>
         </div>
 
-        <div className="expertise-card" data-aos="fade-left" data-aos-duration="1000" data-aos-delay="400">
+        <div
+          className="expertise-card"
+          data-aos="fade-left"
+          data-aos-duration="1000"
+          data-aos-delay="400"
+          data-aos-mirror="true"
+        >
           <div className="card-icon">
             <Smartphone size={40} />
           </div>
-          <h2>Flutter Dev</h2>
+          <h2>Desarrollo web movil</h2>
           <h3>
-            Skilled in developing hybrid mobile apps and cross-platform solutions using the Flutter framework.
+            Especializado en desarrollo de aplicaciones móviles híbridas y
+            soluciones multiplataforma usando frameworks.
           </h3>
         </div>
       </div>
     </section>
   );
 };
-
