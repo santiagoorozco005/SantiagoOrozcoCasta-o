59042a8a119edbc8c9e60017cfeb45d63f23281c
santi orozco <santioro1736@gmail.com>
2024-10-30 00:25:18 -0500

    prueba scroll

---
+++ src/componets/Layouts/Abouy me/About-me.jsx
@@ -0,0 +1,17 @@
+export const AboutMe = () => {
+  return (
+    <>
+      <div data-aos="fade-up">Item 1</div>
+      <div data-aos="fade-right">Item 2</div>
+      <div data-aos="fade-left">Item 3</div>
+      <div
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        data-aos-delay="50"
+        data-aos-easing="ease-in-out"
+      >
+        Item 4
+      </div>
+    </>
+  );
+};
--- src/componets/Pages/Home.jsx
+++ src/componets/Pages/Home.jsx
@@ -1,9 +1,14 @@
+import { AboutMe } from "../Layouts/abouy me/About-me";
 import { Header } from "../Layouts/Header";
 
 
 export const Home = () => {
     return (
+        <>
         <Header />
+        
+        <AboutMe />
+        </>
     );
 };
 
